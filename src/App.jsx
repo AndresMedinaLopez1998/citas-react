d1972dbadb99565be46855bd4ce3654989154a15
AndresMedinaLopez1998 <a.medinalopez@ugto.mx>
2023-04-19 20:14:25 -0600

    Interfaz grafica preparada para meter React

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import Header from "./components/Header";
+import Formulario from "./components/Formulario";
+import ListadoPacientes from "./components/ListadoPacientes";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hola Mundo</h1>
+    <div className="container mx-auto mt-20">
+      <Header />
+
+      <div className="mt-12 lg:flex">
+        <Formulario />
+        <ListadoPacientes />
+      </div>
     </div>
   )
 }
 
-export default App
+export default App;
---
+++ src/components/Paciente.jsx
@@ -0,0 +1,14 @@
+
+const Paciente = () => {
+  return (
+    <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl ">
+        <p className="font-bold mb-3 text-gray-700 uppercase">Mascota: <span className="font-normal normal-case"> Hook</span></p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Propietario: <span className="font-normal normal-case"> Andrés</span></p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Correo: <span className="font-normal normal-case"> a.medinalopez@ugto.mx</span></p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Fecha Alta: <span className="font-normal normal-case"> 10/05/2023</span></p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Sintomas: <span className="font-normal normal-case"> Muy grave del estomago y de la garganta esta ronca</span></p>
+    </div>
+  )
+}
+
+export default Paciente;
